d2ada6b6e6dbfb0ab135c2f003fe6fa0198f4196
Adrian Moncada <adrian.ignaciomoncada@gmail.com>
2021-07-13 19:00:00 -0300

    I include the code for the simpleMemoryGame

--- README.md
+++ README.md
@@ -3,3 +3,34 @@
 This is an entry for The Odin Project's [2021 Game Jam](https://itch.io/jam/top-jam-1). Our team is @Adrian.Moncada, @gabesenacom, and @Ubercj.
 
 The theme is "Edutainment," and we decided to make a memory matching game.
+
+
+simpleMemoryGame
+Create an array with the diferent cards
+
+
+Click on a card and a picture will appear.
+Click on another different card and it will open.
+If the cards match , they remain open , if not they close again.
+Repeat steps 1 and 2 until all cards are open. Then the game ends.
+
+1. Create an array with the card's like items. 
+2. In the array put the name and the img route
+
+Create te simple board UI. For this we will need
+A loop that goes through every element array and creates first  a var to store data for the cards
+a) atributes src for the images
+b) id for the cards
+c) a event when clicked run a function for fliping the card
+
+Check for matches
+1. If the same card is clicked -> alert you click the same card
+2. If two of the same cards are clicked -> alert you found a match!
+3. If none of the above happened -> Try again
+
+Create the function to flip the card
+1. funct flipCard will get the id of the chosen card and reveal the image inside the id
+
+
+
+
---
+++ src/modules/memoryCard.js
@@ -0,0 +1,121 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    const cardArray = [
+        {
+            name: 'animal1',
+            img: 'images/animal1.png'
+        },
+        {
+            name: 'animal1',
+            img: 'images/animal1.png'
+        },
+        {
+            name: 'animal2',
+            img: 'images/animal2.png'   
+        },
+        {
+            name: 'animal2',
+            img: 'images/animal2.png'
+        },
+        {
+            name: 'animal3',
+            img: 'images/animal3.png'
+        },
+        {
+            name: 'animal3',
+            img: 'images/animal3.png'
+        },
+        {
+            name: 'animal4',
+            img: 'images/animal4.png'
+        },
+        {
+            name: 'animal4',
+            img: 'images/animal4.png'
+        },
+        {
+            name: 'animal5',
+            img: 'images/animal5.png'
+        },
+        {
+            name: 'animal5',
+            img: 'images/animal5.png'
+        },
+        {
+            name: 'animal6',
+            img: 'images/animal6.png'
+        },
+        {
+            name: 'animal6',
+            img: 'images/animal6.png'
+        },
+        {
+            name: 'animal7',
+            img: 'images/animal7.png'
+        },
+        {
+            name: 'animal7',
+            img: 'images/animal7.png'
+        },
+    ]
+  cardArray.sort(() => 0.5 - Math.random())
+  
+    const grid = document.querySelector('.grid')
+    const resultDisplay = document.querySelector('#result')
+    var cardsChosen = []
+    var cardsChosenId = []
+    var cardsWon = []
+    //create board
+    function createBoard() {
+        for (let i = 0; i < cardArray.length; i++){
+            var card = document.createElement('img')
+            card.setAttribute('src', 'images/blank.png')
+            card.setAttribute('data-id', i)
+            card.addEventListener('click', flipCard)
+            grid.appendChild(card)
+        }
+    }
+  
+    //check for matches
+    function checkForMatch() {
+        var cards = document.querySelectorAll('img')
+        const optionOneId = cardsChosenId[0]
+        const optionTwoId = cardsChosenId[1]
+        if(optionOneId == optionTwoId) {
+            cards[optionOneId].setAttribute('src', 'images/blank.png')
+            cards[optionTwoId].setAttribute('src', 'images/blank.png')
+            alert('You have clicked the same image!')
+          }
+          else if (cardsChosen[0] === cardsChosen[1]) {
+            alert('You found a match')
+            cards[optionOneId].setAttribute('src', 'images/white.png')
+            cards[optionTwoId].setAttribute('src', 'images/white.png')
+            cards[optionOneId].removeEventListener('click', flipCard)
+            cards[optionTwoId].removeEventListener('click', flipCard)
+            cardsWon.push(cardsChosen)
+          } else {
+            cards[optionOneId].setAttribute('src', 'images/blank.png')
+            cards[optionTwoId].setAttribute('src', 'images/blank.png')
+            alert('Sorry, try again')
+          }
+          cardsChosen = []
+          cardsChosenId = []
+          resultDisplay.textContent = cardsWon.length
+          if  (cardsWon.length === cardArray.length/2) {
+            resultDisplay.textContent = 'Congratulations! You found them all!'
+          }
+        }
+  
+    //flip card
+    function flipCard() {
+        var cardId = this.getAttribute('data-id')
+        cardsChosen.push(cardArray[cardId].name)
+        cardsChosenId.push(cardId)
+        this.setAttribute('src', cardArray[cardId].img)
+        if (cardsChosen.length === 2) {
+            setTimeout(checkForMatch, 500)
+        }
+    }
+  
+    createBoard()
+  })
