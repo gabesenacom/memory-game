63a0d50e364c43209606d9dfbe4f47fae1cabb36
Gabriel Sena <gabesenacom@gmail.com>
2021-07-17 22:51:23 -0300

    Remove unused variables on index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
 import './assets/stylesheets/main.scss';
-import {addPlayer} from './modules/memoryCard';
-import {createPlayer} from './modules/Player';
 import init from './modules/memoryCardController';
 init()
