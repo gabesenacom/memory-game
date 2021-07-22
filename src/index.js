30e888dcf2129ecc64a8e0ba5b015b9d531f3ebc
Chris Jones <ubercj@gmail.com>
2021-07-22 10:53:26 -0400

    Added startup form and connected it to init function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 import './assets/stylesheets/main.scss';
-import init from './modules/memoryCardController';
-init()
+
+// +++++++++++++++++++++ Testing form stuff +++++++++++++++++++++++
+
+import PubSub from 'pubsub-js';
+import TOPIC from './modules/topics';
+import './modules/domController';
+
+const playerForm = document.getElementById('player-form');
+const startButton = document.getElementById('start-game');
+const toggleRulesButtons = document.querySelectorAll('.toggle-rules');
+const playerList = [];
+
+playerForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+  let data = {
+    event: event,
+    playerList: playerList
+  }
+  PubSub.publish(TOPIC.SUBMIT_PLAYER_FORM, data);
+})
+
+startButton.addEventListener('click', () => {
+  PubSub.publish(TOPIC.START_GAME, playerList);
+})
+
+toggleRulesButtons.forEach((button) => {
+  button.addEventListener('click', () => {
+    PubSub.publish(TOPIC.SHOW_RULES);
+  })
+})
--- src/modules/domController.js
+++ src/modules/domController.js
@@ -2,6 +2,9 @@ import PubSub from 'pubsub-js';
 import TOPIC from './topics';
 import { createElement } from './utils';
 import { Game } from './memoryCardController';
+import init from './memoryCardController';
+
+// Card DOM
 
 function buildCard (topic, card) {
   let className = 'card';
@@ -28,6 +31,11 @@ function buildFlippableCard (topic, card) {
   cardDOM.addEventListener('click', Game.flipCardEvent);
 }
 
+PubSub.subscribe(TOPIC.BUILD_CARD, buildCard);
+PubSub.subscribe(TOPIC.BUILD_FLIPPABLE_CARD, buildFlippableCard);
+
+// Player DOM
+
 function buildPlayer (topic, data) {
   let player = {
     name: data.player.name,
@@ -38,6 +46,60 @@ function buildPlayer (topic, data) {
   data.card.buildPlayer(player)
 }
 
-PubSub.subscribe(TOPIC.BUILD_CARD, buildCard);
-PubSub.subscribe(TOPIC.BUILD_FLIPPABLE_CARD, buildFlippableCard);
 PubSub.subscribe(TOPIC.BUILD_PLAYER, buildPlayer);
+
+// Startup Form + Rules
+
+const gameRules = document.getElementById('rules');
+const playerDisplay = document.getElementById('player-list');
+const playerForm = document.getElementById('player-form');
+const startupForm = document.getElementById('startup');
+const errors = document.getElementById('errors');
+const gameDisplay = document.getElementsByTagName('main')[0];
+
+function submitPlayerForm (topic, data) {
+  let { event, playerList } = data;
+  let playerName = event.target.elements.playerName.value;
+  let playerType = event.target.elements.playerType.checked ? 'Computer': 'Human';
+
+  playerList.push({
+      name: playerName,
+      type: event.target.elements.playerType.checked
+    });
+
+  let newPlayer = createElement('p', 'player-name', playerDisplay);
+  newPlayer.textContent = `Name: ${playerName} -- Type: ${playerType}`;
+
+  clearFields(event);
+}
+
+function clearFields (event) {
+  event.target.elements.playerName.value = '';
+  event.target.elements.playerType.checked = false;
+}
+
+function removeChildren (element) {
+  while (element.firstChild) { element.lastChild.remove() };
+}
+
+function startGame (topic, playerList) {
+  removeChildren(errors);
+
+  if (playerList.length < 2) { 
+    let errorMessage = createElement('p', 'error', errors);
+    errorMessage.textContent = 'You must have at least 2 players to play!';
+    return;
+  }
+
+  gameDisplay.classList.remove('hidden');
+  startupForm.classList.add('hidden');
+  init(playerList);
+}
+
+function showRules (topic) {
+  gameRules.classList.toggle('hidden');
+}
+
+PubSub.subscribe(TOPIC.SUBMIT_PLAYER_FORM, submitPlayerForm);
+PubSub.subscribe(TOPIC.START_GAME, startGame);
+PubSub.subscribe(TOPIC.SHOW_RULES, showRules);
