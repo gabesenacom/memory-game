f3417b51420f49d895e3c9ef3b73c743a208156e
Gabriel <gabesenacom@gmail.com>
2021-07-25 21:34:36 -0300

    Remove clearFields; Add sortRandomArray; Add getNextItemPositionInArray

--- src/modules/utils.js
+++ src/modules/utils.js
@@ -1,22 +1,27 @@
 export const createElement = (tag, className, parentNode = null) => {
   let element = document.createElement(tag)
   element.className = className
-  if(parentNode)
-    parentNode.appendChild(element)
+  if (parentNode) parentNode.appendChild(element)
   return element
 }
 
-export function clearFields (event) {
-  event.target.elements.playerName.value = '';
-  event.target.elements.playerType.checked = false;
-}
-
 export function removeChildren (element) {
-  while (element.firstChild) { element.lastChild.remove() };
+  while (element.firstChild) {
+    element.lastChild.remove()
+  }
 }
 
 export function clickOutsideElement (event, elementList, elementToHide) {
   if (!elementList.includes(event.target)) {
-    elementToHide.classList.add('hidden');
+    elementToHide.classList.add('hidden')
   }
+}
+
+export function sortRandomArray (array) {
+  return array.sort(() => 0.5 - Math.random())
+}
+
+export function getNextItemPositionInArray (array, position) {
+  let max = array.length - 1
+  return position === max ? 0 : ++position
 }
