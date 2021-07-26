e468b972fdda2acecb46dc15cfbdf7a4f5f427cf
Gabriel <gabesenacom@gmail.com>
2021-07-25 21:22:36 -0300

    Add condition to check if playerList has 4 or more players. If yes, don't accept new player request. ## Check it later

--- src/modules/homePage.js
+++ src/modules/homePage.js
@@ -1,41 +1,46 @@
-import PubSub from 'pubsub-js';
-import TOPIC from './topics';
-import './domController';
+import PubSub from 'pubsub-js'
+import TOPIC from './topics'
+import './domController'
 
-const playerForm = document.getElementById('player-form');
-const startButton = document.getElementById('start-game');
-const showRulesButton = document.querySelector('.show-rules');
-const currentIcon = document.getElementById('current-icon');
-const selectedIcon = document.getElementById('playerIcon');
-const iconChoices = document.querySelectorAll('.icon-choice');
-const playerList = [];
+const playerForm = document.getElementById('player-form')
+const startButton = document.getElementById('start-game')
+const showRulesButton = document.querySelector('.show-rules')
+const currentIcon = document.getElementById('current-icon')
+const selectedIcon = document.getElementById('playerIcon')
+const iconChoices = document.querySelectorAll('.icon-choice')
+const playerList = []
 
 export function homePageInit () {
-  playerForm.addEventListener('submit', (event) => {
-    event.preventDefault();
+  playerForm.addEventListener('submit', event => {
+    event.preventDefault()
+    if (playerList.length >= 4) {
+      // SEND LOG MAX PLAYERS REACH
+      return
+    }
+
     let data = {
       event: event,
       playerList: playerList
     }
-    PubSub.publish(TOPIC.SUBMIT_PLAYER_FORM, data);
+    PubSub.publish(TOPIC.SUBMIT_PLAYER_FORM, data)
   })
-  
+
   startButton.addEventListener('click', () => {
-    PubSub.publish(TOPIC.START_GAME, playerList);
+    PubSub.publish(TOPIC.START_GAME, playerList)
   })
 
   showRulesButton.addEventListener('click', () => {
-    PubSub.publish(TOPIC.SHOW_RULES);
+    PubSub.publish(TOPIC.SHOW_RULES)
   })
 
   currentIcon.addEventListener('click', () => {
-    PubSub.publish(TOPIC.SHOW_ICON_CHOICES);
+    PubSub.publish(TOPIC.SHOW_ICON_CHOICES)
   })
-  
-  iconChoices.forEach((choice) => {
+
+  iconChoices.forEach(choice => {
     choice.addEventListener('click', () => {
-      currentIcon.src = choice.src;
-      selectedIcon.value = choice.src;
+      currentIcon.src = choice.src
+      selectedIcon.value = choice.src
     })
   })
 }
