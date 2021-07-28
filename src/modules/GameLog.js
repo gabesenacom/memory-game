908fbe85617516ee07e540c743ed90cf01d6e1e9
Gabriel <gabesenacom@gmail.com>
2021-07-27 23:21:53 -0300

    Add GameLog.js with the log content

---
+++ src/modules/GameLog.js
@@ -0,0 +1,62 @@
+import PubSub from 'pubsub-js'
+import TOPIC from './topics'
+
+const _createLogDOMElement = () => {
+  let element = document.createElement('p')
+  element.classList.add('hidden')
+  document.body.appendChild(element)
+  return element
+}
+
+const logDOM = _createLogDOMElement()
+
+let pendingLogs = []
+
+function sendLogMessage(logDOM, type, message) {
+  clearPendingLogs()
+  logDOM.classList.remove("hidden")
+  logDOM.setAttribute("data-type", type)
+  logDOM.setAttribute("id", 'log')
+  logDOM.textContent = message
+  let pendingLog = setTimeout(() => logDOM.classList.add("hidden"), 4000)
+  pendingLogs.push(pendingLog)
+}
+
+function clearPendingLogs() {
+  pendingLogs.forEach((pendingLog) => clearTimeout(pendingLog))
+}
+
+
+/* expect type structure
+  type    = value
+  info    = 1
+  success = 2
+  warning = 3
+  error   = 4
+*/
+
+function convertTypeToStringType(type_num) {
+  switch(type_num) {
+    case 2:
+      return 'success'
+    case 3:
+      return 'warning'
+    case 4:
+      return 'error'
+  }
+  return 'info'
+}
+
+/* expect data structure
+  {
+    type: undefined(not set or undefined), 1, 2, 3, 4
+    message: log message in strings: 'log message'
+  }
+*/
+function handleLogMessage(topic, data) {
+  let type = convertTypeToStringType(data.type)
+  sendLogMessage(logDOM, type, data.message)
+}
+
+
+PubSub.subscribe(TOPIC.SEND_LOG, handleLogMessage)
