abf72173a30a0a14e3008502310373e5dcdb36d4
Gabriel <gabesenacom@gmail.com>
2021-07-31 20:39:58 -0300

    Change path of animal images

--- src/modules/cardImages.js
+++ src/modules/cardImages.js
@@ -5,7 +5,7 @@ function addAnimalImage (src) {
 }
 
 for (let i = 1; i <= 7; i++) {
-  addAnimalImage(`images/animal${i}.png`)
+  addAnimalImage(`images/animals/animal-${i}.svg`)
 }
 
 export default cardImages
