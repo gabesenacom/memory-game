6d2dd97aa4702173e60d4971a8f3276287104eb7
Gabriel <gabesenacom@gmail.com>
2021-08-04 20:11:59 -0300

    Change close button icon to 'fa-times'

--- src/modules/domController.js
+++ src/modules/domController.js
@@ -170,7 +170,7 @@ function _createPlayerListDOM (newPlayer, playerList, addButton) {
   let newPlayerName = createElement('p', null)
   let newPlayerIcon = createElement('img', null)
   let newPlayerType = createElement('p', null)
-  let removePlayerButton = createElement('i', 'fas fa-ban')
+  let removePlayerButton = createElement('i', 'fas fa-times')
 
   newPlayerName.textContent = newPlayer.name
   newPlayerIcon.src = newPlayer.icon
